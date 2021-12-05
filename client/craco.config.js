0b6bd521717c295016b575aa9755ee8a57e359ef
Stephen Gladney <stephengladney@gmail.com>
2021-12-05 15:24:15 -0500

    Get Tailwind working locally

---
+++ client/craco.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+}
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react"
 import logo from "./logo.svg"
 import "./App.css"
+import { getCookie } from "./lib/cookies"
 
 function App() {
-  const [isLoggedIn, toggleIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [token, setToken] = useState(getCookie("fmntoken"))
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div className="bg-red-400">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
