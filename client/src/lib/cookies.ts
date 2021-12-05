51165fe2748a9e1f00130a56c8e797bc052bd406
Stephen Gladney <stephengladney@gmail.com>
2021-12-05 14:11:10 -0500

    Add setCookie function

--- client/src/lib/cookies.ts
+++ client/src/lib/cookies.ts
@@ -13,3 +13,10 @@ export function getCookie(cname: string) {
   }
   return ""
 }
+
+export function setCookie(cname: string, cvalue: string, exdays: number) {
+  var d = new Date()
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000)
+  var expires = "expires=" + d.toUTCString()
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/"
+}
