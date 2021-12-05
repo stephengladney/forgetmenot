6b4c85a096f99f4db1cebedae3d3fda4f5fe349c
Stephen Gladney <stephengladney@gmail.com>
2021-12-05 15:56:55 -0500

    Add header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,23 +8,11 @@ function App() {
   const [token, setToken] = useState(getCookie("fmntoken"))
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="bg-red-400">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gray-800">
+      <div className={"bg-indigo-800 text-white p-4 text-left"}>
+        ForgetMeNot
+      </div>
+      dfgdfgdfg
     </div>
   )
 }
