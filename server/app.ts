c9117102e652b49ea53b97f11e90dffed9f98354
Stephen Gladney <stephengladney@gmail.com>
2021-12-05 20:13:52 -0500

    get typescript to compile and build for server

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,9 @@
+require("dotenv").config()
 import express from "express"
 
 const app = express()
 
 app.listen(5000, () => {
   console.log("ForgetMeNot server is running!")
+  console.log(process.env.HELLO_VAR)
 })
