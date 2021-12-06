d88e45206eff1c480c437e0438542b8a1d6db890
Stephen Gladney <stephengladney@gmail.com>
2021-12-05 20:50:44 -0500

    organize app for server. install node path

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,15 @@
 require("dotenv").config()
 import express from "express"
+import { resolve } from "path"
+
+import sequelize from "./config/sequelize"
+import routes from "./routes/"
 
 const app = express()
+sequelize.sync()
+
+app.use(express.static(resolve(__dirname, "../../client/build")))
+app.use(routes)
 
 app.listen(5000, () => {
   console.log("ForgetMeNot server is running!")
